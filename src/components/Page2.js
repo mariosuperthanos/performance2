adcc705fcc2fef83c8ef1c21a1dd9ea28bff5003
[Mario David] <[mario.bargianu@gmail.com]>
2024-10-13 17:26:21 +0300

    update

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -4,7 +4,7 @@ import favicon from '../favicon.ico';
 const Page2 = ({ onRouteChange }) => (
   <div className='App'>
     <header>
-       
+      <img src={favicon} className='App-logo' alt='logo'></img>
       <h1 className='App-title'>Welcome to react</h1>
     </header>
     <button onClick={() => onRouteChange('page1')}>Page1</button>
